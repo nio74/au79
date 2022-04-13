9144691b28dbf507c6d240800a8cda61cecce9b2
nio74 <mpaolo74@gmail.com>
2022-04-13 14:43:50 +0200

    LavorDragAreaOk

--- lib/screen/home_Screen.dart
+++ lib/screen/home_Screen.dart
@@ -60,102 +60,107 @@ class _HomeScreenState extends State<HomeScreen> with WindowListener {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                onWindowClose();
-              },
-              icon: const Icon(
-                Icons.close,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: const  Icon(
-                Icons.call_to_action_outlined,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.account_balance_sharp,
-              )),
-        ],
-        title:const  Text('Au79'),
-      ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('riparazioni'),
-              onTap: () {
-                Navigator.pushNamed(context, '/riparazioni');
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
+    return GestureDetector(
+      onPanStart: (details) {
+        windowManager.startDragging();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  onWindowClose();
+                },
+                icon: const Icon(
+                  Icons.close,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.call_to_action_outlined,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.account_balance_sharp,
+                )),
           ],
+          title: const Text('Au79'),
         ),
-      ),
-      body: DefaultTabController(
-          length: 6,
-          child: Scaffold(
-            body: NestedScrollView(
-              floatHeaderSlivers: false,
-              headerSliverBuilder:
-                  (BuildContext context, bool innerBoxIsScrolled) {
-                return <Widget>[
-                  new SliverAppBar(
-                    //title: Text('Tabs Demo'),
-                    toolbarHeight: 10,
-                    pinned: false,
-                    floating: false,
-                    bottom: TabBar(
-                      isScrollable: false,
-                      tabs: [
-                        Tab(
-                          child: Text('Vendita'),
-                        ),
-                        Tab(child: const Text('Oreficeria')),
-                        Tab(child: const Text('Orologeria')),
-                        Tab(child: const Text('Gioielleria')),
-                        Tab(child: const Text('Argernteria')),
-                        Tab(child: const Text('Riparazioni')),
-                      ],
-                    ),
-                  ),
-                ];
-              },
-              body: TabBarView(
-                children: [
-                  Icon(Icons.flight, size: 350),
-                  Icon(Icons.directions_transit, size: 350),
-                  Icon(Icons.directions_car, size: 350),
-                  Icon(Icons.directions_bike, size: 350),
-                  Icon(Icons.directions_boat, size: 350),
-                  Icon(Icons.directions_boat, size: 350),
-                ],
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
               ),
-            ),
-          )),
+              ListTile(
+                title: const Text('riparazioni'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/riparazioni');
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: DefaultTabController(
+            length: 6,
+            child: Scaffold(
+              body: NestedScrollView(
+                floatHeaderSlivers: false,
+                headerSliverBuilder:
+                    (BuildContext context, bool innerBoxIsScrolled) {
+                  return <Widget>[
+                    new SliverAppBar(
+                      //title: Text('Tabs Demo'),
+                      toolbarHeight: 10,
+                      pinned: false,
+                      floating: false,
+                      bottom: TabBar(
+                        isScrollable: false,
+                        tabs: [
+                          Tab(
+                            child: Text('Vendita'),
+                          ),
+                          Tab(child: const Text('Oreficeria')),
+                          Tab(child: const Text('Orologeria')),
+                          Tab(child: const Text('Gioielleria')),
+                          Tab(child: const Text('Argernteria')),
+                          Tab(child: const Text('Riparazioni')),
+                        ],
+                      ),
+                    ),
+                  ];
+                },
+                body: TabBarView(
+                  children: [
+                    Icon(Icons.flight, size: 350),
+                    Icon(Icons.directions_transit, size: 350),
+                    Icon(Icons.directions_car, size: 350),
+                    Icon(Icons.directions_bike, size: 350),
+                    Icon(Icons.directions_boat, size: 350),
+                    Icon(Icons.directions_boat, size: 350),
+                  ],
+                ),
+              ),
+            )),
+      ),
     );
     /* Scaffold(
       appBar: AppBar(
