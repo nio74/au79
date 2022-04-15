2c19a7a93cef5530ec528ffd2dadfa7ade7dd7e6
nio74 <mpaolo74@gmail.com>
2022-04-15 07:49:39 +0200

    casa07_49

--- lib/page/riparazioni_Page.dart
+++ lib/page/riparazioni_Page.dart
@@ -1,38 +1,21 @@
 import 'package:au79/model/model_Roparazioni.dart';
 import 'package:flutter/material.dart';
 
-class RiparazioniPage extends StatefulWidget {
-  RiparazioniPage({Key? key}) : super(key: key);
+class RiparazionePage extends StatefulWidget {
+  RiparazionePage({Key? key}) : super(key: key);
 
   @override
-  State<RiparazioniPage> createState() => _PageRiparazioniState();
+  State<RiparazionePage> createState() => _PageRiparazioniState();
 }
 
-class _PageRiparazioniState extends State<RiparazioniPage> {
+class _PageRiparazioniState extends State<RiparazionePage> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Column(),
-              Stack(
-                children: [sectionBuste(context)],
-              ),
-              Column()
-            ],
-          ),
-          Row()
-        ],
-      ),
-    );
+    return Container(child: sectionBuste(context));
   }
 
   Widget sectionBuste(BuildContext context) {
     ListView.builder(
-        padding: EdgeInsets.all(10),
-        shrinkWrap: false,
         itemCount: envelopes.length,
         itemBuilder: (context, index) {
           return Card(
