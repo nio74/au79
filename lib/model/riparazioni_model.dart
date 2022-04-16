445fa2dc2c7fc7b5e805f490cdef64844f2e6229
nio74 <mpaolo74@gmail.com>
2022-04-16 07:25:51 +0200

    Casa 16_04

---
+++ lib/model/riparazioni_model.dart
@@ -0,0 +1,205 @@
+class RiparazioniModel {
+  final int codice;
+  final String nome;
+  final String oggetti;
+  final String lavoriFaFare;
+
+  RiparazioniModel(
+      {required this.codice,
+      required this.nome,
+      required this.oggetti,
+      required this.lavoriFaFare});
+}
+
+final riparazioniLista = [
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1,
+      nome: "Gino",
+      oggetti: "1 anello 2 orecchini 1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 2,
+      nome: "Paolo",
+      oggetti: "1 bracciale",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 3, nome: "Sole", oggetti: "1 orologio", lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 4,
+      nome: "Luna",
+      oggetti: "1 orecchino",
+      lavoriFaFare: " saldare"),
+  RiparazioniModel(
+      codice: 1, nome: "MArio", oggetti: "1 tennis", lavoriFaFare: " saldare"),
+];
