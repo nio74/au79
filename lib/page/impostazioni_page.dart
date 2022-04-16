ce294e129850033b75296ca0df9088d95e4c311a
nio74 <mpaolo74@gmail.com>
2022-04-16 07:50:32 +0200

    r1

--- lib/page/impostazioni_page.dart
+++ lib/page/impostazioni_page.dart
@@ -1,5 +1,7 @@
+import 'package:au79/utilities/config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 class ImpostazioniPage extends StatefulWidget {
   const ImpostazioniPage({Key? key}) : super(key: key);
@@ -14,55 +16,26 @@ class _ImpostazioniPageState extends State<ImpostazioniPage> {
     return Scaffold(
         appBar: AppBar(title: const Text("Impostazioni")),
         body: ListView(
-          children: const <Widget>[
+          children: <Widget>[
             Card(
               child: ListTile(
                 leading: FlutterLogo(),
                 title: Text('Cambia colore thema'),
-                trailing: ElevatedButton(onPressed: () async {
-                ThemeMode newThemeMode =
-                    sharedConfig.themeMode == ThemeMode.light
-                        ? ThemeMode.dark
-                        : ThemeMode.light;
+                trailing: ElevatedButton(
+                    onPressed: () async {
+                      ThemeMode newThemeMode =
+                          sharedConfig.themeMode == ThemeMode.light
+                              ? ThemeMode.dark
+                              : ThemeMode.light;
 
-                await sharedConfigManager.setThemeMode(newThemeMode);
-                await windowManager.setBrightness(
-                  newThemeMode == ThemeMode.light
-                      ? Brightness.light
-                      : Brightness.dark,
-                );
-              },, child: Text("switch dark/Light")),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: FlutterLogo(),
-                title: Text('One-line with both widgets'),
-                trailing: Icon(Icons.more_vert),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text('One-line dense ListTile'),
-                dense: true,
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: FlutterLogo(size: 56.0),
-                title: Text('Two-line ListTile'),
-                subtitle: Text('Here is a second line'),
-                trailing: Icon(Icons.more_vert),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: FlutterLogo(size: 72.0),
-                title: Text('Three-line ListTile'),
-                subtitle:
-                    Text('A sufficiently long subtitle warrants three lines.'),
-                trailing: Icon(Icons.more_vert),
-                isThreeLine: true,
+                      await sharedConfigManager.setThemeMode(newThemeMode);
+                      await windowManager.setBrightness(
+                        newThemeMode == ThemeMode.light
+                            ? Brightness.light
+                            : Brightness.dark,
+                      );
+                    },
+                    child: Text("switch dark/Light")),
               ),
             ),
           ],
